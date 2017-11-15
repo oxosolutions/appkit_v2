68e9d07b782a0f75b7a3f1d36345c0e654fc18b6
Rahul Sharma <rahulsharma841990@outlook.com>
2017-11-15 17:48:00 +0530

    sqlite

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,38 +13,78 @@ import { Platform } from 'ionic-angular';
 })
 export class HomePage {
 
+  public db;
+
   constructor(public navCtrl: NavController,public sqlite:SQLite, public platform:Platform) {
   	
-
-
   	 	platform.ready().then(() => {
-  	 // 	if(this.platform.is('mobileweb')){
-  		// 	console.log('on browser');
+    	 // 	if(this.platform.is('mobileweb')){
+    		// 	console.log('on browser');
 
-  		// }         
-  		if(this.platform.is('cordova')){
-  			console.log("on mobile");
-  			this.sqlite.create({
-		  		name: 'test.db',
-		  		location : 'default'
-	  		});
+    		// }         
+    		if(this.platform.is('cordova')){
+    			console.log("on mobile");
+    			this.sqlite.create({
+  		  		name: 'test.db',
+  		  		location : 'default'
+  	  		});
+   
+    		}else{
 
-  		}else{
-  			var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
-			db.transaction(function (tx) {  
-			   tx.executeSql('CREATE TABLE IF NOT EXISTS LOGS (id unique, log)');
-			});
-  		}
-        });
+    			this.db = openDatabase('Appkit', '1.0', 'Test DB', 2 * 1024 * 1024);
+
+           
+
+
+    			// this.db.transaction(function (tx) {  
+    			//    tx.executeSql('CREATE TABLE IF NOT EXISTS Users (id unique, username)');
+       //       tx.executeSql('INSERT INTO Users (id, username) VALUES (1, "foobar")');
+       //       tx.executeSql('INSERT INTO Users (id, username) VALUES (2, "HEDER")');
+             
+    			// });
+    		}
+    });
 
 
 
 
   	
   }
+
+
+
   pushPage(){
     this.navCtrl.push( LoginPage );
   }
+  create(){
+    this.db.transaction((tx) => {
+       tx.executeSql("CREATE TABLE IF NOT EXISTS attendance (id, firstname)");
+    });
+    console.log('Created');
+  }
+  insert(){
+    this.db.transaction((tx)=>{
+      tx.executeSql('INSERT INTO attendance (id, firstname) VALUES (1, "foobar")');
+     console.log('data is inserted');
+    });
+  }
+  select(){
+    this.db.transaction((tx)=>{
+      tx.executeSql('SELECT * FROM attendance',[],function(tx,result){
+          if(result.rows.length > 0){
+            for(var i=0; i < result.rows.length; i++ ){
+             // console.log(result.rows);
+             var data=result.rows.item(i);
+             console.log(data.id);
+              //console.log(result.rows.item(i).id);
+            }
+           
+          }
+      });
+     
+    })
+  }
+   
 
-
-}
+ 
+}  
