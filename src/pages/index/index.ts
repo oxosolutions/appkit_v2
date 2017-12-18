70dfcf3d7cd948c3e22022d65397d18ea5288025
radha226 <radhasingh226@gmail.com>
2017-12-18 15:45:23 +0530

    loader in ts file

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,9 +1,9 @@
 import { Component,ViewChild } from '@angular/core';
+import { LoadingController } from 'ionic-angular';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { ProductPage } from '../product/product';
 import { HomePage } from '../home/home';
-import { ServiceProvider } from '../../providers/service/service';
 import {PracticeProvider} from '../../providers/practice/practice';
 import { Events } from 'ionic-angular';
 
@@ -15,10 +15,11 @@ import { Events } from 'ionic-angular';
 })
 export class IndexPage {
   
- AppkitProducts:any;
- metadata:any;  
+AppkitProducts:any;
+metadata:any;  
+AppkitPage:any;
    
-constructor(public navCtrl: NavController, public events: Events, public navParams: NavParams, public pracProvider : PracticeProvider) {
+constructor(public navCtrl: NavController, public loadingctrl: LoadingController, public events: Events, public navParams: NavParams, public pracProvider : PracticeProvider) {
     //this.loadPeople();
     this.events.publish('hello','paul','radha');
     this.getData();       
@@ -29,21 +30,41 @@ getData(){
   let products = 'app_products';
   let metadata = 'meta_data';
   let dd = 'database';
-  
- this.pracProvider.SelectMeta(dd,metadata).then(result=>{
-     this.metadata=result;
-     console.log(this.metadata);
+
+let loading=this.loadingctrl.create({
+  content: `
+      <div class="custom-spinner-container">
+      <ion-spinner name="circles">Wait...</ion-spinner>
+      </div>`
  });
- this.pracProvider.SelectProducts(dd,products).then(result=>{
-    this.AppkitProducts=result;
-    console.log(this.AppkitProducts);
- })
- this.pracProvider.SelectPages(dd,pages).then(result=>{
-    this.AppkitPage=result;
-   console.log(this.AppkitPage);
- });
-  
+loading.present();
+
+
+     
+    //return new Promise((resolve, reject)=>{
+      this.pracProvider.SelectMeta(dd,metadata).then(result=>{
+      this.metadata=result;
+      });
+      this.pracProvider.SelectProducts(dd,products).then(result=>{
+      this.AppkitProducts=result;
+      console.log(this.AppkitProducts);
+      })
+      this.pracProvider.SelectPages(dd,pages).then(result=>{
+      this.AppkitPage=result;
+      console.log(this.AppkitPage);
+      });
+
+   
+  // DataLoad(){
+  //   this.alldata();
+  //   // this.alldata().then(result1=>{
+  //   //   loading.dismiss();
+  //   // });
+
+  // } 
 }
+
+
 
 
   
