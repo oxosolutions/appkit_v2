81590981e20d6d264c1de9ee3982a79b2cfd4d71
radha226 <radhasingh226@gmail.com>
2017-12-18 16:39:04 +0530

    changes to ts file of index

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -24,54 +24,48 @@ constructor(public navCtrl: NavController, public loadingctrl: LoadingController
     this.events.publish('hello','paul','radha');
     this.getData();       
 }
-
 getData(){
   let pages = 'app_pages';
   let products = 'app_products';
   let metadata = 'meta_data';
   let dd = 'database';
 
-let loading=this.loadingctrl.create({
-  content: `
-      <div class="custom-spinner-container">
-      <ion-spinner name="circles">Wait...</ion-spinner>
-      </div>`
- });
-loading.present();
-
-
-     
-    //return new Promise((resolve, reject)=>{
-      this.pracProvider.SelectMeta(dd,metadata).then(result=>{
-      this.metadata=result;
-      });
-      this.pracProvider.SelectProducts(dd,products).then(result=>{
-      this.AppkitProducts=result;
-      console.log(this.AppkitProducts);
-      })
-      this.pracProvider.SelectPages(dd,pages).then(result=>{
-      this.AppkitPage=result;
-      console.log(this.AppkitPage);
-      });
-
-   
-  // DataLoad(){
-  //   this.alldata();
-  //   // this.alldata().then(result1=>{
-  //   //   loading.dismiss();
-  //   // });
-
-  // } 
+  let loading=this.loadingctrl.create({
+    content: `
+        <div class="custom-spinner-container">
+        <ion-spinner name="circles">Wait...</ion-spinner>
+        </div>`
+   });
+  loading.present();
+  this.selectData(pages,products,metadata,dd).then(result=>{
+    loading.dismiss();
+    console.log(result.metadata);
+  });
 }
 
-
-
-
+selectData(pages,products,metadata,dd){
+  return new Promise((resolve,reject)=>{
+    this.pracProvider.SelectMeta(dd,metadata).then(result=>{
+      this.metadata=result;
+      this.pracProvider.SelectProducts(dd,products).then(result=>{
+        this.AppkitProducts=result;
+        this.pracProvider.SelectPages(dd,pages).then(result=>{
+          this.AppkitPage=result;
+          let collection = {};
+          collection['metadata'] = this.metadata;
+          collection['AppkitProducts'] = this.AppkitProducts;
+          collection['AppkitPage'] = this.AppkitPage;
+          resolve(collection);
+        });
+      })
+    });
+  });
+  
+}
   
 
 ionViewDidLoad() {
-   //console.log(this.name);
-     console.log('ionViewDidLoad IndexPage');
+  console.log('ionViewDidLoad IndexPage');
 }
 
 }
