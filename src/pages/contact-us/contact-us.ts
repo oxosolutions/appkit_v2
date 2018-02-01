46e5be85caa17a6cc2f80969ebf82293c2d941e1
oxosolutions <radhasingh226@gmail.com>
2018-02-01 15:15:28 +0530

    changes

--- src/pages/contact-us/contact-us.ts
+++ src/pages/contact-us/contact-us.ts
@@ -44,7 +44,12 @@ export class ContactUsPage {
           quality: 100,
           destinationType: this.camera.DestinationType.DATA_URL,
           encodingType: this.camera.EncodingType.JPEG,
-          mediaType: this.camera.MediaType.PICTURE
+          mediaType: this.camera.MediaType.PICTURE,
+          allowEdit: true,
+          correctOrientatin: true,
+          targetWidth: 400,
+          targetHeight: 250,
+          // saveToPhotoAlbum: false
           //saveToPhotoAlbum: false
 
         };
@@ -53,7 +58,7 @@ export class ContactUsPage {
          .then((imageData)=>{
            console.log('end take picture');
             this.htmlImageFromCamera = "data:image/jpeg;base64," + imageData;
-            
+
           })
           .catch(err=>{
             console.log(err);
