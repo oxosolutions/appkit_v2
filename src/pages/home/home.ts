9bcb5a3ca051736927349752f56ebf446c53ffe5
oxosolutions <radhasingh226@gmail.com>
2018-05-03 10:30:59 +0530

    add home page code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,81 +7,72 @@
    import { Events } from 'ionic-angular';
    import { Pipe, PipeTransform } from '@angular/core';
 
-   @Component({
-     templateUrl: 'home.html',
-     selector: 'page-home',
-   })
-   export class HomePage {
-      name:any;
-      Db;
-      slughome;
-      AppkitProducts:any;
-      metadata:any;  
-      AppkitPage=[];
-      resultData:any;
-      loading:any;
-      database;
-      Pagesid:any;
-      apppages:any;
-      content:any;
- 
-
-   constructor(public events: Events,public sqlite: SQLite,public platform:Platform,public navParams: NavParams,public navCtrl: NavController, public loadingctrl:LoadingController , private modalctrl:ModalController, public dbprovider:DatabaseProvider) {
-   }  
-   getData(){
+  @Component({
+    templateUrl: 'home.html',
+    selector: 'page-home',
+  })
+  export class HomePage {
+    name:any;
+    Db;
+    slughome;
+    AppkitProducts:any;
+    metadata:any;  
+    AppkitPage=[];
+    resultData:any;
+    loading:any;
+    database;
+    Pagesid:any;
+    apppages:any;
+    content:any;
+  constructor(public events: Events,public sqlite: SQLite,public platform:Platform,public navParams: NavParams,public navCtrl: NavController, public loadingctrl:LoadingController , private modalctrl:ModalController, public dbprovider:DatabaseProvider) {
+  }  
+  getData(){
       this.selectData().then(result=>{
          this.resultData=result;
          if(this.resultData.apppages!=undefined){
+           console.log(this.resultData.apppages);
          }
-        // console.log(this.resultData.apppages);
-        let data="yessall"
          this.events.publish('user:created', this.resultData.AppkitPage); 
-         this.content=this.resultData.slughome.content;
-        
-        //console.log(this.resultData.AppkitPage);
-         //this.inject=this.resultData.slughome;
+         this.content=this.resultData.slughome.content; 
+          //console.log(this.resultData.AppkitPage);
+          //this.inject=this.resultData.slughome;
          
-
-      },);
-   }
-   selectData(){
-   	//console.log('select page call from  home')
+      });
+  }
+  selectData(){
       return new Promise((resolve,reject)=>{
           let i;
-         // console.log('event select');
           this.dbprovider.SelectMeta('Meta').then((result)=>{
            this.metadata=result;
-          // console.log(this.metadata);
             this.dbprovider.SelectPages('app_pages').then((resultpages:any)=>{
                //console.log(resultpages);
                this.Pagesid=this.navParams.get('id');
 
                for(i=0; i < resultpages.rows.length; i++){
-
                   resultpages[i] = resultpages.rows.item(i);
                    
                     for(let keypages in resultpages[i]){
                       let json:any;
-                       if(keypages=='id' || keypages =='show_in_menu'){                           
-                              resultpages[i][keypages]=resultpages[i][keypages];
+                      if(keypages=='id' || keypages =='show_in_menu'){                           
+                          resultpages[i][keypages]=resultpages[i][keypages];
                               //console.log(json);
-                        }else{
-                          resultpages[i][keypages]=resultpages[i][keypages].replace(/&lt;/g, "<")
-                          .replace(/&gt;/g, ">")
-                          .replace(/&quot;/g, '"')
-                          .replace(/&#039;/g, "'");
-                        }
+                      }else{
+                        resultpages[i][keypages]=resultpages[i][keypages].replace(/&lt;/g, "<")
+                        .replace(/&gt;/g, ">")
+                        .replace(/&quot;/g, '"')
+                        .replace(/&#039;/g, "'");
+                      }
                     }
                      //console.log(resultpages[i]);
-                     this.AppkitPage.push(resultpages[i]);
-                     //console.log(this.AppkitPage);
-                     if(resultpages[i].slug=="home-page"){
+                    this.AppkitPage.push(resultpages[i]);
+                    console.log(this.AppkitPage);
+                    if(resultpages[i].slug=="home-page"){
                          this.slughome=resultpages[i];
-                     }
-                     if(resultpages[i].id==this.Pagesid){
+                    }
+                    if(resultpages[i].id==this.Pagesid){
                           this.apppages=resultpages[i];
                         //  console.log(this.apppages);
-                     } 
+                    } 
                   }
                 //  console.log('ddkjfdfj');
                  //this.AppkitPage=resultpages;
@@ -92,34 +83,20 @@
                  collection['metadata']=this.metadata;               
                 resolve(collection);
                 resolve(this.slughome);
-           });
+            });
          });
          
       })
-   }
+  }
 
-   refreshPage(){
-       //window.location.reload();
-       // this.dbprovider.delall().then((result)=>{
-
-       // });
-    //   this.loading=this.loadingctrl.create({
-  //           content:'index page..'
-      //    });
-      // this.loading.present();
-       this.dbprovider.DeleteAll().then(result=>{  
-           //this.dbprovider.Apitable1().then((dd)=>{
-             this.navCtrl.setRoot(MyApp);
-          // })
-          
-          //this.datacall();
-         
-       });
-   }
-   
-   ionViewDidLoad(){
+  refreshPage(){
+    this.dbprovider.DeleteAll().then(result=>{  
+      this.navCtrl.setRoot(MyApp);
+    });
+  }
+  ionViewDidLoad(){
     this.getData();         
-   }
+  }
    
 }
 
